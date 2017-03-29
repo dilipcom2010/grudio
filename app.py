ef641099a63bccecc28b5b957a2c42a19e6116c3
Dilip Kumar <dilip@machinehappy.com>
2017-03-29 11:46:10 +0530

    more freedom in radio url

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ class Application(tornado.web.Application, AppHelper):
 			(r"/songs/addNew/([0-9]+)", songs.AddNewSong),
 			(r"/songs/verify", songs.OnSongVerify),
 			(r'/songs/(.*)', tornado.web.StaticFileHandler, {'path': songs_path}),
-			(r"/radio/([a-z-]+)/([0-9]+)", radio.Home),
+			(r"/radio/([0-9a-z-]+)/([0-9]+)", radio.Home),
 			(r"/radio", radio.Home),
 			(r"/radio/playlist", radio.Playlist),
 			(r"/radio/playlist/polling", radio.PlaylistPolling),
